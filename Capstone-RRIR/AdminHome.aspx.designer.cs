c86c1d21aaad1e5df114f136efdcef4f2316dfd4
basketbwall <55670468+basketbwall@users.noreply.github.com>
2021-11-16 20:11:35 -0500

    Redid the footer. added final link to the admin home dropdown

--- Capstone-RRIR/AdminHome.aspx.designer.cs
+++ Capstone-RRIR/AdminHome.aspx.designer.cs
@@ -13,14 +13,5 @@ namespace Capstone_RRIR
 
     public partial class WebForm11
     {
-
-        /// <summary>
-        /// btnNextPage control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlButton btnNextPage;
     }
 }
